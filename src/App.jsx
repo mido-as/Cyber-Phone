fa8b30e820a9c0e3d43a3816217eac8d543ac8cc
mido-as <mido11dragon@gmail.com>
2026-04-02 15:26:55 +0200

    Props added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Footer from "./components/Footer"
 
 
 function App() {
+  
+
   return (
    <>
       <Header/>
@@ -12,10 +14,24 @@ function App() {
       <div className="container d-flex flex-column align-items-start gap-4 p-4">
         <h3>Related Products</h3>
         <div className="d-flex col-10 justify-content-between align-items-center gap-5">
-          <Collection/>
-          <Collection/>
-          <Collection/>
-          <Collection/>
+          <Collection productName = "Apple iPhone 17 Pro Orange"
+            productCode = "(MQ233)" 
+            productPrice = "1000 $" 
+            img = "https://static.reach-tele.com/uploads/thumbs/1a/1a686ddea41de0779e66d3953a9418a7.png"/>
+          <Collection productName = "Oneplus 15 with Snapdragon 8 Elite Gen 5"
+            productCode = "(MQ243)" 
+            productPrice = "700 $" 
+            img = "https://image01-in.oneplus.net/media/202511/06/4fd767bdb010e0b78639871524cce2a5.png?x-amz-process=image/format,webp/quality,Q_80"/>
+          <Collection productName = "Tecno Camon 40"
+            productCode = "(MQ273)" 
+            productPrice = "500 $" 
+            img = "https://www.atom.com.mm/uploads/ATOM-2025/devicebundle/Camon40_308PX-x-308PX-id.png"/>
+          <Collection productName = "Samsung Galaxy S26 Ultra 5G"
+            productCode = "(MQ238)" 
+            productPrice = "900 $" 
+            img = "https://myxprs.com/cdn/shop/files/SamsungGalaxyS26Ultra5G12GBRam512GBxprs.webp?v=1772620020"/>
+
+          
       </div>
       </div>
       <Footer/> 
--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -1,16 +1,17 @@
 import './style.css'
 
-function Collection() {
+function Collection(props) {
   return (
    <div id='productCard' className='col-4 d-flex justify-content-between flex-column align-items-center p-4 shadow'>
 
      <div className='col-12 rounded bg-dark d-flex justify-content-between flex-column align-items-center p-2'>
-       <img src="https://m.media-amazon.com/images/I/71b3HI25YgL._AC_SX679_.jpg" alt=" " />
+       <img src= {props.img} alt=" " />
      </div>
 
       <div className='d-flex flex-column justify-content-start align-items-start'>
-        <h6>Nothing Phone (3a) 5G </h6>
-        <h6>(MQ233)</h6>
+        <h6>{props.productName} </h6>
+        <h6>{props.productCode}</h6>
+        <h6>{props.productPrice}</h6>
       </div>
       <button className="btn btn-dark ps-5 pe-5">Add To Cart</button>
 
